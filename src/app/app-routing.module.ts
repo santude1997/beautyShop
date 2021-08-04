ef84e5e9589c7f07ad4c715301a247b91cea08d2
santude1997 <santude1997@outlook.com>
2021-08-04 08:45:35 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { CartComponent } from './cart/cart.component';
+
+const routes: Routes = [
+  {path:'',component:HomeComponent,pathMatch:'full'},
+  {path:'home',component:HomeComponent},
+  {path:'product',component:ProductComponent},
+  {path:'cart',component:CartComponent}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
